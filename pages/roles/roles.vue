57e8cd815b4b849b88860cd1133ea84e07e38385
小黎（家） <22708206@qq.com>
2024-05-03 21:33:29 +0800

    feat(pages/role): 新增角色页面及功能
    新增角色页面及功能

--- pages/roles/roles.vue
+++ pages/roles/roles.vue
@@ -1,21 +1,360 @@
 <template>
-	<view>
-	</view>
+    <el-scrollbar v-loading="loading" class="roles page">
+        <view style="margin: 20px;">
+            <el-button type="primary" @click="openRoleDialog(null)">新增角色</el-button>
+        </view>
+        <el-table :data="list" row-key="_id" :tree-props="{ children: 'children' }" default-expand-all border>
+            <el-table-column label="" width="50px" align="center" />
+            <el-table-column prop="sort" label="排序" align="center" width="60px" />
+            <el-table-column prop="avatar" label="头像" align="center" min-width="60px">
+                <template #default="{ row }">
+                    <div style="display: flex;justify-content: center">
+                        <el-image v-if="row.avatar" :src="row.avatar" :preview-src-list="[row.avatar]" preview-teleported fit="contain" style="border-radius: 50%;" />
+                    </div>
+                </template>
+            </el-table-column>
+            <el-table-column prop="avatar" label="背景" align="center" min-width="60px">
+                <template #default="{ row }">
+                    <div style="display: flex;justify-content: center">
+                        <el-image v-if="row.avatar_long" :src="row.avatar_long" :preview-src-list="[row.avatar_long]" preview-teleported fit="contain" />
+                    </div>
+                </template>
+            </el-table-column>
+            <el-table-column prop="name" label="名称" align="center" min-width="100px" />
+            <el-table-column prop="user_name" label="用户" align="center" min-width="100px" />
+            <el-table-column prop="category_id" label="分类" align="center" min-width="100px">
+                <template #default="{ row }">
+                    <div>
+                        <el-tag>{{ categoryObj[row.category_id] || row.name }}</el-tag>
+                    </div>
+                </template>
+            </el-table-column>
+            <el-table-column prop="desc" label="简介" align="center" min-width="160px" />
+            <el-table-column prop="tag_list" label="标签" align="center" min-width="100px">
+                <template #default="{ row }">
+                    <div>
+                        <el-tag v-for="(item, index) in row.tag_list" :key="index" type="warning" style="margin: 4px auto;">{{ item }}</el-tag>
+                    </div>
+                </template>
+            </el-table-column>
+            <el-table-column prop="gender" label="性别" align="center" min-width="80px">
+                <template #default="{ row }">
+                    <div>
+                        <el-tag type="success">{{ { '1': '男', '2': '女', '': '未知' }[row.gender] }}</el-tag>
+                    </div>
+                </template>
+            </el-table-column>
+            <el-table-column prop="category_id" label="风格" align="center" width="60px">
+                <template #default="{ row }">
+                    <div style="display: flex;justify-content: center">
+                        <div :class="`tag tag-${ row.styles }`" />
+                    </div>
+                </template>
+            </el-table-column>
+            <el-table-column prop="prompt" label="提示词" align="center" min-width="160px" />
+            <el-table-column prop="guide_list" label="引导语" align="center" min-width="160px" :formatter="(e) => e.guide_list.join(';')" />
+            <el-table-column prop="show" label="启用" align="center" width="80px">
+                <template #default="{ row }">
+                    <div>
+                        <el-switch v-model="row.show" :disabled="true" />
+                    </div>
+                </template>
+            </el-table-column>
+            <el-table-column prop="hot_count" label="热度" align="center" min-width="60px" />
+            <el-table-column prop="talk_count" label="对话" align="center" min-width="60px" />
+            <el-table-column prop="update_time" label="更新时间" align="center" min-width="100px" :formatter="(e) => dayjs(e.create_time).format('MM-DD HH:mm:ss')" />
+            <el-table-column prop="create_time" label="注册时间" align="center" min-width="100px" :formatter="(e) => dayjs(e.create_time).format('MM-DD HH:mm:ss')" />
+            <el-table-column label="操作" align="center" width="160" fixed="right">
+                <template #default="{row}">
+                    <el-button type="primary" @click="openRoleDialog(row)">修改</el-button>
+                    <el-button type="danger" @click="deleteRole(row)">删除</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+        <view class="pagination">
+            <el-pagination
+                    v-model:currentPage="listParams.pageNo"
+                    v-model:page-size="listParams.pageSize"
+                    :total="listParams.total"
+                    @size-change="changePage(1, $event)"
+                    @current-change="changePage" :page-sizes="[10, 20, 50, 100, 200, 500]"
+                    :layout="'total, sizes, prev, pager, next'"
+                    small
+            />
+        </view>
+
+        <el-dialog class="dialog" v-model="roleShow" width="720px" :title="roleData._id ? '新增角色' : '修改角色'" align-center draggable>
+            <el-form ref="roleRef" class="role-form" :model="roleData" :rules="roleRules" label-width="100px">
+                <div style="display: flex;">
+                    <el-form-item label="角色名称" prop="name">
+                        <el-input v-model="roleData.name" :maxlength="10" placeholder="请输入角色名称" clearable show-word-limit />
+                    </el-form-item>
+                    <el-form-item label="用户名称" prop="user_name">
+                        <el-input v-model="roleData.user_name" :maxlength="10" placeholder="请输入用户名称" clearable show-word-limit />
+                    </el-form-item>
+                </div>
+
+                <el-form-item label="角色分类" prop="category_id">
+                    <el-select v-model="roleData.category_id" placeholder="请选择角色分类" :disabled="roleData._id && roleData.category_id === 'null' && roleData.children.length" clearable>
+                        <el-option v-for="item in categoryList" :key="item._id" :label="item.name" :value="item._id" />
+                        <el-option label="一级分类" value="null" />
+                    </el-select>
+                </el-form-item>
+
+                <div style="display: flex;">
+                    <el-form-item label="角色头像" prop="avatar">
+                        <div style="position: relative;width: 50px;height: 50px;padding: 4px; border: 1px dashed #DCDFE6; display: flex;justify-content: center;align-items: center;">
+                            <img v-if="roleData.avatar" :src="roleData.avatar" style="width: 100%; max-width: 50px;max-height: 50px;object-fit: contain;border-radius: 50%;" alt=""/>
+                            <div v-else style="font-size: 24px;color: #DCDFE6;">+</div>
+                            <button @click="uploadImg('avatar')" style="position: absolute;width: 100%;height: 100%; z-index: 10000;inset: 0;opacity: 0" />
+                        </div>
+                    </el-form-item>
+                    <el-form-item label="角色背景" prop="avatar_long">
+                        <div style="position: relative;width: 50px;height: 50px;padding: 4px;border: 1px dashed #DCDFE6; display: flex;justify-content: center;align-items: center;">
+                            <img v-if="roleData.avatar_long" :src="roleData.avatar_long" style="width: 100%; max-width: 50px;max-height: 50px;object-fit: contain;border-radius: 4px;" alt=""/>
+                            <div v-else style="font-size: 24px;color: #DCDFE6;">+</div>
+                            <button @click="uploadImg('avatar_long')" style="position: absolute;width: 100%;height: 100%; z-index: 10000;inset: 0;opacity: 0" />
+                        </div>
+                    </el-form-item>
+                </div>
+
+                <el-form-item label="角色简介" prop="desc">
+                    <el-input type="textarea" v-model="roleData.desc" :rows="3" :maxlength="200" placeholder="请输入角色简介" clearable show-word-limit />
+                </el-form-item>
+
+                <el-form-item label="角色标签" prop="tag_list">
+                    <div style="display: flex;">
+                        <el-input v-model="roleData.tag_list[0]" :rows="3" :maxlength="4" placeholder="标签1" clearable show-word-limit />
+                        <div style="width: 10px;flex-shrink: 0"></div>
+                        <el-input v-model="roleData.tag_list[1]" :rows="3" :maxlength="4" placeholder="标签2" clearable show-word-limit />
+                        <div style="width: 10px;flex-shrink: 0"></div>
+                        <el-input v-model="roleData.tag_list[2]" :rows="3" :maxlength="4" placeholder="标签3" clearable show-word-limit />
+                        <div style="width: 10px;flex-shrink: 0"></div>
+                        <el-input v-model="roleData.tag_list[3]" :rows="3" :maxlength="4" placeholder="标签4" clearable show-word-limit />
+                    </div>
+                </el-form-item>
+
+                <div style="display: flex;">
+                    <el-form-item label="角色性别" prop="gender">
+                        <el-radio-group v-model="roleData.gender">
+                            <el-radio value="">未知</el-radio>
+                            <el-radio value="1">男</el-radio>
+                            <el-radio value="2">女</el-radio>
+                        </el-radio-group>
+                    </el-form-item>
+                    <el-form-item label="角色风格" prop="styles">
+                        <el-radio-group v-model="roleData.styles">
+                            <el-radio v-for="item in 4" :key="item" :value="item">
+                                <div :class="`tag tag-${item}`" />
+                            </el-radio>
+                        </el-radio-group>
+                    </el-form-item>
+                </div>
+
+                <el-form-item label="角色提示词" prop="prompt">
+                    <el-input type="textarea" v-model="roleData.prompt" :rows="3" :maxlength="200" placeholder="请输入角色提示词" clearable show-word-limit />
+                </el-form-item>
+                <el-form-item label="角色引导语" prop="guide_list">
+                    <el-input v-model="roleData.guide_list[0]" :maxlength="100" placeholder="请输入角色引导语" clearable show-word-limit />
+                </el-form-item>
+
+                <div style="display: flex;">
+                    <el-form-item label="角色排序" prop="sort">
+                        <el-input-number v-model="roleData.sort" :min="0" :max="1000" :precision="0" :step="1" controls-position="right" />
+                    </el-form-item>
+                    <el-form-item label="是否启用" prop="show">
+                        <el-switch v-model="roleData.show" />
+                    </el-form-item>
+                </div>
+            </el-form>
+
+            <template #footer>
+                <div class="pb-[12px]">
+                    <el-button type="info" plain @click="roleShow = false">取消</el-button>
+                    <el-button class="!ml-[14px]" type="primary" @click="saveRole">确认</el-button>
+                </div>
+            </template>
+        </el-dialog>
+    </el-scrollbar>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
+<script setup>
+import { onMounted, reactive, ref } from 'vue'
+import { dayjs, ElMessage, ElMessageBox } from 'element-plus'
+import {createAvatarKey, listToTree} from '../../utils/common'
 
-			}
-		},
-		methods: {
+/* 传统数据库集合 */
+const db = uniCloud.database()
+const rolesDb = db.collection('roles')
 
-		}
-	}
+const loading = ref(false)
+const listParams = reactive({ pageNo: 1, pageSize: 50, total: 0 })
+const list = ref([])
+const categoryList = ref([])
+const categoryObj = ref([])
+
+const roleDataDefault = () => ({
+    category_id: 'null',
+    sort: 0,
+    show: false,
+    name: '',
+    user_name: '',
+    avatar: '',
+    avatar_long: '',
+    gender: '',
+    desc: '',
+    tag_list: [],
+    styles: 1,
+    prompt: '',
+    guide_list: [],
+    hot_count: 0,
+    talk_count: 0,
+    update_time: ''
+})
+const roleShow = ref(false)
+const roleData = ref(roleDataDefault())
+
+const roleRef = ref();
+const roleRules = reactive({
+    name: [{ required: true, message: '请填写角色名称', trigger: 'change' }],
+    user_name: [{ required: true, message: '请填写用户名称', trigger: 'change' }],
+    category_id: [{ required: true, message: '请选择角色分类', trigger: 'change' }],
+    desc: [{ required: true, message: '请填写角色简介', trigger: 'change' }],
+    prompt: [{ required: true, message: '请填写角色提示词', trigger: 'change' }],
+    tag_list: [{ required: true, message: '请至少填写一个标签', trigger: 'change' }, { validator: (r, v, cb) => v.filter(i => i).length === 0 ? cb(new Error('请填写引导语')) : cb(), trigger: 'change' }],
+    guide_list: [{ required: true, message: '请填写引导语', trigger: 'change' }, { validator: (r, v, cb) => !v[0] ? cb(new Error('请填写引导语')) : cb(), trigger: 'change' }],
+})
+
+const getList = async () => {
+    const start = (listParams.pageNo - 1) * listParams.pageSize
+    loading.value = true
+    const { result: { data, count } } = await rolesDb.skip(start).limit(listParams.pageSize).orderBy('create_time desc').get({ getCount:true })
+    loading.value = false
+
+    if (!data) return
+    list.value = listToTree(data)
+    listParams.total = count
+
+    categoryList.value = []
+    categoryObj.value = {}
+    list.value.forEach(i => {
+        categoryList.value.push({ _id: i._id, name: i.name })
+        categoryObj.value[i._id] = i.name
+    })
+
+}
+
+const changePage = async (e) => {
+    await getList()
+}
+
+const openRoleDialog = (row) => {
+    roleShow.value = true
+    roleData.value = row ? JSON.parse(JSON.stringify(row)) : roleDataDefault()
+    setTimeout(() => roleRef.value.clearValidate())
+}
+
+const uploadImg = (avatar) => {
+    uni.chooseImage({
+        count: 1, //默认9
+        sizeType: ['original'], //可以指定是原图还是压缩图，默认二者都有
+        sourceType: ['album'], //从相册选择
+        success: function (res) {
+            const filePath = res.tempFilePaths[0]
+            const file = res.tempFiles[0]
+
+            uni.showLoading({ title: '头像上传中' })
+            const suffix = file.name.split('.').pop()
+            const cloudPath = `/img/roles/${createAvatarKey()}.${ suffix }`
+
+            uniCloud.uploadFile({
+                filePath,
+                cloudPath,
+                cloudPathAsRealPath: true,
+                success: (res) => {
+                    roleData.value[avatar] = res.fileID
+                },
+                complete: () => uni.hideLoading()
+            });
+        }
+    });
+}
+
+const saveRole = async () => {
+    if (!roleRef.value) return;
+    const valid = await roleRef.value.validate().catch(() => ({}));
+    if (valid !== true) return ElMessage.warning('请填写信息')
+
+    const params = JSON.parse(JSON.stringify(roleData.value))
+    const id = params._id
+    delete params._id
+    delete params.children
+
+    /* 更新时间,多加10s */
+    params.update_time = dayjs().add(10, 'second').valueOf()
+
+    /* 过滤无效标签 */
+    params.tag_list = params.tag_list.filter(i => i)
+
+    const { errMsg } = id ? await rolesDb.doc(id).update(params).catch(e => e) : await rolesDb.add(params).catch(e => e)
+    if (errMsg) return ElMessage.error(errMsg)
+
+    ElMessage.success('更新成功')
+
+    roleShow.value = false
+
+    await getList()
+}
+
+const deleteRole = ({ children, _id }) => {
+    if (children.length) return ElMessage.warning('该分类下还有其他应用，禁止删除！')
+
+    ElMessageBox.confirm('确定删除吗?', '删除角色', { confirmButtonText: '确定', cancelButtonText: '取消', type: 'warning' })
+            .then(async () => {
+                await rolesDb.doc(_id).remove();
+                ElMessage.success('删除成功')
+                await getList()
+            })
+
+}
+
+onMounted(async () => await getList())
 </script>
 
-<style>
+<style lang="scss" scoped>
+.roles {
+    min-height: 100vh;
 
+    .tag {
+        width: 36px;
+        height: 20px;
+        border-radius: 4px;
+
+        &.tag-1 {
+            background: linear-gradient(180deg, #FFFFFF 0%, #E7D4FF 100%);
+        }
+
+        &.tag-2 {
+            background: linear-gradient(180deg, #FFFFFF 0%, #FFF8C6 100%);
+        }
+
+
+        &.tag-3 {
+            background: linear-gradient(180deg, #FFFFFF 0%, #CDE1FD 100%);
+        }
+
+        &.tag-4 {
+            background: linear-gradient(180deg, #FFFFFF 0%, #FFD7ED 100%);
+        }
+
+    }
+
+    :deep .dialog {
+         .el-radio {
+            margin-right: 18px;
+        }
+    }
+}
 </style>
+
+
