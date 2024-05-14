bc6552574a382958ebf361144ad76d2e0dd516ca
小黎 <22708206@qq.com>
2024-05-14 10:09:06 +0800

    feat(pages/user): 增加平台枚举，优化平台信息展示
    增加平台枚举，优化平台信息展示

---
+++ config/enums.js
@@ -0,0 +1,26 @@
+/**
+ * @file enums.ts 枚举配置
+ * @description 相关枚举值
+ * @author xiao li
+ * @copyright 采黎AI
+ * @createDate 2024-05-11 12:05
+ */
+
+export const platformEnums = {
+	juejin: '掘金',
+	csdn: 'CSDN',
+	v2ex: 'V2EX',
+	w2solo: 'W2Solo',
+	xiaozhong: '小众软件',
+	xioahongshu: '小红书',
+	gongzhonghao: '公众号',
+	pengyouqaun: '朋友圈',
+	poster: '主海报',
+	wxad: '微信搜索广告',
+	'wxad-show': '微信展示广告'
+}
+
+export  const genderEnums = {
+	1: '/static/icon/men.png',
+	2: '/../static/icon/women.png'
+}
